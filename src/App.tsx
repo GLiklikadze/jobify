1fc0e5f1f69daf5ed93ff45f8368341b295fa9f5
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2025-01-03 19:49:52 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>JOBIFY</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  const rootPath = path.resolve(process.cwd());
+  const srcPath = `${rootPath}/src`;
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "~": rootPath,
+        "@": srcPath,
+      },
+    },
+  };
+});
